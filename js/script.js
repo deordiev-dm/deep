2ae9509a7cc16c6debabe2b90231ec0f0b3e929d
deordiev-dm <93293534+deordiev-dm@users.noreply.github.com>
2023-06-11 15:04:05 +0300

    1st polished vers
    impovments in adapted layout
    smaller images
    appearence after loading

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 "use strict";
 
+const bodyElement = document.body;
+
+window.addEventListener("load", pageLoad);
+function pageLoad(e) {
+	bodyElement.classList.add("loaded");
+}
+
 const portfolioItems = document.querySelectorAll(".portfolio__item");
 
 portfolioItems.forEach((portfolioItem) => {
