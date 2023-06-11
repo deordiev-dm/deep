1cdf7d75e0584d74eae71665d343cc2d9a774a58
deordiev-dm <93293534+deordiev-dm@users.noreply.github.com>
2023-06-11 23:22:06 +0300

    4th? versiom
    animation improved for intro, about.
    added data-attr for header anim
    first version of services aniim

--- js/script.js
+++ js/script.js
@@ -27,32 +27,48 @@ function pageLoad(e) {
 	// header apearence after loading
 	const pageHeaderElement = document.querySelector("header");
 	pageHeaderElement.classList.add("load");
+	const pageHeaderElementLogo = document.querySelector(".header__logo");
+	const pageHeaderLogoAnimationDelay = pageHeaderElementLogo.dataset.animDelay;
+	pageHeaderElementLogo.style.transitionDelay = `${+pageHeaderLogoAnimationDelay}ms`;
 
 	const bodyIntro = document.querySelector(".body-intro");
 	bodyIntro.classList.add("load");
+	const bodyIntroAnimationDelay = bodyIntro.dataset.animDelay;
+	bodyIntro.style.transitionDelay = `${+bodyIntroAnimationDelay}ms`;
 
 	// .intro__image
 	const bodyIntroImage = document.querySelector(".intro__image");
 	bodyIntroImage.classList.add("load");
+	const bodyIntroImageAnimationDelay = bodyIntroImage.dataset.animDelay;
+	bodyIntroImage.style.transitionDelay = `${+bodyIntroImageAnimationDelay}ms`;
 
 	// animation for other elements
 	// Intersection Observer API
 	const bodyAboutSect = document.querySelector(".about__body");
-
+	const bodyServices = document.querySelector(".services__body");
+	const imagesServices = document.querySelector(".services__images");
 	let options = {
 		root: null,
-		rootMargin: "0px",
-		threshold: 0.3,
+		rootMargin: "50px",
+		threshold: 0,
 	};
 
 	let callback = (entries, observer) => {
 		entries.forEach((entry) => {
 			if (entry.isIntersecting) {
-				bodyAboutSect.classList.add("intersecting");
+				if (entry.target.classList.contains("about__body")) {
+					bodyAboutSect.classList.add("intersecting");
+				} else if (entry.target.classList.contains("services__body")) {
+					bodyServices.classList.add("intersecting");
+				} else if (entry.target.classList.contains("services__images")) {
+					imagesServices.classList.add("intersecting");
+				}
 			}
 		});
 	};
 
 	let observer = new IntersectionObserver(callback, options);
 	observer.observe(bodyAboutSect);
+	observer.observe(bodyServices);
+	observer.observe(imagesServices);
 }
