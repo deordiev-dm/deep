b2f8a1b34a608a933b1f3fdfe2d9c9463e432c2e
deordiev-dm <93293534+deordiev-dm@users.noreply.github.com>
2023-06-11 15:52:59 +0300

    2nd version
    added more animatoins

--- js/script.js
+++ js/script.js
@@ -3,21 +3,37 @@
 const bodyElement = document.body;
 
 window.addEventListener("load", pageLoad);
+
 function pageLoad(e) {
+	// increasing opacity to 1 after loading
 	bodyElement.classList.add("loaded");
-}
 
-const portfolioItems = document.querySelectorAll(".portfolio__item");
+	// hover effect for portflio items
+	// scaling starts from the opposite point of mouse entry
+	const portfolioItems = document.querySelectorAll(".portfolio__item");
+	portfolioItems.forEach((portfolioItem) => {
+		portfolioItem.addEventListener("mouseenter", addTransformOrigin);
+	});
 
-portfolioItems.forEach((portfolioItem) => {
-	portfolioItem.addEventListener("mouseenter", addTransformOrigin);
-});
+	function addTransformOrigin(event) {
+		const image = event.target.querySelector("img");
+		const itemWidth = event.target.offsetWidth;
+		const itemHeight = event.target.offsetHeight;
+		image.style.transformOrigin = `${itemWidth - event.offsetX}px ${
+			itemHeight - event.offsetY
+		}px`;
+	}
 
-function addTransformOrigin(event) {
-	const image = event.target.querySelector("img");
-	const itemWidth = event.target.offsetWidth;
-	const itemHeight = event.target.offsetHeight;
-	image.style.transformOrigin = `${itemWidth - event.offsetX}px ${
-		itemHeight - event.offsetY
-	}px`;
+	// header apearence after loading
+	const pageHeaderElement = document.querySelector("header");
+	pageHeaderElement.classList.add("load");
+
+	const bodyIntro = document.querySelector(".body-intro");
+	bodyIntro.classList.add("load");
+
+	// .intro__image
+	const bodyIntroImage = document.querySelector(".intro__image");
+	bodyIntroImage.classList.add("load");
+
+	// animation for other elements
 }
